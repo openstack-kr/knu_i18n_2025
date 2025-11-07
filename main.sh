4c8b154281e273cce93d70f56792d96178d3b7b4
LeeJuyeong <163111542+ale8ander@users.noreply.github.com>
2025-11-07 09:56:19 +0900

    Update main.sh: correct critical typo

--- main.sh
+++ main.sh
@@ -21,7 +21,7 @@ python translate.py \
   --glossary_dir ./glossary \
   --example_dir ./po-example \
   --pot_url "https://tarballs.opendev.org/openstack/translation-source/swift/master/releasenotes/source/locale/releasenotes.pot" \
-  --target_pot_file "i18n-docs-source-locale-part.pot" \
+  --target_pot_file "i18n-docs-source-locale.pot" \
   --glossary_url "https://opendev.org/openstack/i18n/raw/commit/129b9de7be12740615d532591792b31566d0972f/glossary/locale/{lang}/LC_MESSAGES/glossary.po" \
   --glossary_po_file "glossary.po" \
   --glossary_json_file "glossary.json" \
